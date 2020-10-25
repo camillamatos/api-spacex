4a2cee8abb01997ae75704b1d63413102ed21621
Camilla Matos <camillapozerm@gmail.com>
2020-10-25 15:06:50 -0300

    api

--- src/shared/infra/server.ts
+++ src/shared/infra/server.ts
@@ -4,13 +4,17 @@ import cors from 'cors'
 import routes from './routes'
 import { MongoHelper } from './mongo-helper'
 
-(async () => {
-  await MongoHelper.connect('mongodb+srv://dbSpaceX:spacex@cluster0.vsp1e.mongodb.net/dbSpaceX?retryWrites=true&w=majority')
-  const app = express()
-  app.use(cors())
-  app.use(express.json())
-  app.use(routes)
+const start = async () => {
+  try {
+    await MongoHelper.connect('mongodb+srv://dbSpaceX:spacex@cluster0.vsp1e.mongodb.net/dbSpaceX?retryWrites=true&w=majority')
+    const app = express()
+    app.use(cors())
+    app.use(express.json())
+    app.use(routes)
+    app.listen(process.env.PORT || 3333, () => {console.log('Server running on port 3333')})
+  } catch (error) {
+    console.log(error)
+  }
+}
 
-
-  app.listen(process.env.PORT || 3333, () => {console.log('Server running on port 3333')})
-})()
+start()
