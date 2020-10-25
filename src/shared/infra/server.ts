68d086ae9debb994a8038c2bb737e3155f739a92
Camilla Matos <camillapozerm@gmail.com>
2020-10-25 14:58:57 -0300

    api

--- src/shared/infra/server.ts
+++ src/shared/infra/server.ts
@@ -4,11 +4,13 @@ import cors from 'cors'
 import routes from './routes'
 import { MongoHelper } from './mongo-helper'
 
-MongoHelper.connect('mongodb+srv://dbSpaceX:spacex@cluster0.vsp1e.mongodb.net/dbSpaceX?retryWrites=true&w=majority')
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.use(routes)
+(async () => {
+  await MongoHelper.connect('mongodb+srv://dbSpaceX:spacex@cluster0.vsp1e.mongodb.net/dbSpaceX?retryWrites=true&w=majority')
+  const app = express()
+  app.use(cors())
+  app.use(express.json())
+  app.use(routes)
 
 
-app.listen(process.env.PORT, () => {console.log('Server running on port 3333')})
+  app.listen(process.env.PORT, () => {console.log('Server running on port 3333')})
+})()
